3e7acbf4b5efc4c8253dad98afbe254ad0c28a75
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2021-01-07 01:00:42 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,29 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
 	@Transactional(readOnly = true) //evitar lock de escrita no banco
 	public List<OrderDTO> findAll() {
 		List<Order> list = orderRepository.findOrdersWithProducts();
 		
 		return list.stream().map(item -> new OrderDTO(item)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId()); // nao vai no banco aqui. cria uma entidade gerenciavel pelo JPA pra depois no metodo save salvar as associações
+			
+			order.getProducts().add(product);
+		}
+		
+		order = orderRepository.save(order);
+		
+		return new OrderDTO(order);
 	}
 }
