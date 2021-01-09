9cf27ab090b3dc9061cdd255ee24e69094959c6b
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2021-01-09 14:45:22 -0300

    Working on responsiveness

--- front-web/src/components/Home/styles.ts
+++ front-web/src/components/Home/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components'
 export const Container = styled.main`
   ${({theme}) => css`
     background-color: ${theme.colors.white};
-    height: calc(100vh - 254px);
+    height: calc(100vh - 7.3rem - 8.5rem);
   `}
 `;
 
@@ -20,27 +20,62 @@ export const Wrapper = styled.div`
     width: 100%;
     min-width: 360px;
   }
+
+  @media (max-width: 870px) {
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    svg {
+      max-height: 360px;
+    }
+  }
+
+  @media (max-width: 420px) {
+    svg {
+      margin-bottom: 2rem;
+    }
+  }
+
 `;
 
 export const Actions = styled.aside`
   max-width: 52rem;
+
+  @media (max-width: 870px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const Title = styled.h1`
   ${({theme}) => css`
-    font-size: ${theme.font.sizes.xxlarge};
+    font-size:  min(${theme.font.sizes.xxlarge}, 10vw);
     color: ${theme.colors.secondary};
-    line-height: 55px;
+    line-height: 5.5rem;
     margin-bottom: 4.2rem;
   `}
+
+  @media (max-width: 870px) {
+    line-height: 4.5rem;
+    margin-top: 2rem;
+    margin-bottom: 2.4rem;
+    text-align: center;
+  }
 `;
 
 export const Description = styled.div`
   ${({theme}) => css`
-    font-size: ${theme.font.sizes.xlarge};
+    font-size: min(${theme.font.sizes.xlarge}, 5vw);
     color: ${theme.colors.gray};
     margin-bottom: 4.2rem;
   `}
+
+  @media (max-width: 870px) {
+    line-height: 2.4rem;
+    margin-bottom: 3.6rem;
+    text-align: center;
+  }
 `;
 
 export const OrderButton = styled.a`
@@ -53,4 +88,14 @@ export const OrderButton = styled.a`
     border-radius: ${theme.border.radius};
     text-decoration: none;
   `}
+
+  @media (max-width: 870px) {
+    align-self: center;
+    padding: 1rem 2rem;
+  }
+
+  @media (max-width: 420px) {
+    margin-top: -2rem;
+    margin-bottom: 2rem;
+  }
 `;
