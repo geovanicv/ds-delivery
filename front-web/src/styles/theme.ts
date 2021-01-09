2e86da1abc014f661abdb66c6c2faa0faf7d3532
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2021-01-09 18:31:29 -0300

    Exclude spacings of theme file

--- front-web/src/styles/theme.ts
+++ front-web/src/styles/theme.ts
@@ -6,13 +6,15 @@ export default {
     family:
       "Open Sans, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     normal: 400,
-    bold: 600,
+    semiBold: 600,
+    bold: 700,
     sizes: {
       xsmall: '1.2rem',
       small: '1.4rem',
       medium: '1.6rem',
       large: '1.8rem',
-      xlarge: '2.4rem'
+      xlarge: '2.4rem',
+      xxlarge: '5.6rem'
     }
   },
   colors: {
@@ -22,14 +24,5 @@ export default {
     lightGray: '#F5F5F5',
     gray: '#9E9E9E',
     green: '#008E5B'
-  },
-  spacings: {
-    xxsmall: '0.8rem',
-    xsmall: '1.6rem',
-    small: '2.4rem',
-    medium: '3.2rem',
-    large: '4.0rem',
-    xlarge: '4.8rem',
-    xxlarge: '5.6rem'
   }
 } as const
